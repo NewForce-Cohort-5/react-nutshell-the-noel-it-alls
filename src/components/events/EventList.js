9974bd6721b7ddf97ab43b162c02641d010f3ef4
john gallagher <127highstreet@gmail.com>
2021-12-17 11:25:50 -0500

    Tommy's solution to the events sorted by month in eventlist

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -4,13 +4,12 @@ import { useNavigate } from "react-router-dom";
 import { EventContext } from "./EventProvider"
 import { EventCard } from "./EventCard"
 import "./Event.css"
-import {Substring} from 'react-substring';
+// import {Substring} from 'react-substring';
 
 export const EventList = () => {
   // This state changes when `getAnimals()` is invoked below
   const { events, getEvents } = useContext(EventContext)
   const navigate = useNavigate()
-  const substring = Substring ()
     
   //useEffect - reach out to the world for something
   useEffect(() => {
@@ -32,23 +31,37 @@ export const EventList = () => {
         <button id="save-button" className="btn-secondary"onClick={() => {navigate("/events/create")}}>
             Add Event
         </button>
-        <div className="eventbyMonth" >   
-        <ul><p>Sorted by Months</p>
+        <div  >   
+        <div><p>Sorted by Months</p>
              {months.map(m =>(
-            <li key={m.id} value={m.id}>{m.name}:{events.filter(event => event.eventDate.substring(5,6) === m.id)}</li>))}
-         </ul>
-         {console.log("events by dec", events.filter(event => event.eventDate.substring(5,6) === 12))}
+               
+             events.map(singleEvent => {
+               
+               console.log(new Date(singleEvent.eventDate).getMonth())
+              if( (m.id-1) === new Date(singleEvent.eventDate).getMonth()) {  
+                return ( <>
+                  <h3 key={m.id} value={m.id}>{m.name}</h3>
+                 <EventCard event={singleEvent} />
+                 </>
+                )
+              }
+              }
+              )
+            
+            ))}
+         </div>
+         {/* {console.log{"events }y dec", events.filter(event => event.eventDate.data.Substring(5,7) === 12))} */}
       </div> 
       <div className="eventList">
         {console.log("EventList: Render", events)}
-
+{/* 
       {eventsByUser.length !== 0 ?eventsByUser.map(event => {
           return <EventCard 
                   key={event.id} 
                   event={event} 
                   />
         }):<p>No events listed yet</p>
-      }
+      } */}
 
       </div>
     </div>
