125a3e0239e7f46bf12eb5777fa4f2100720e170
john gallagher <127highstreet@gmail.com>
2021-12-16 21:33:40 -0500

    working on the select option

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -8,6 +8,8 @@ import "./Event.css"
 export const EventList = () => {
   // This state changes when `getAnimals()` is invoked below
   const { events, getEvents } = useContext(EventContext)
+  const { sortedEventsByMonth, setsortedEventsByMonth} = useState ({})
+
   const navigate = useNavigate()
   //useEffect - reach out to the world for something
   useEffect(() => {
@@ -16,12 +18,14 @@ export const EventList = () => {
   }, [])
 
 
-  const eventsByUser = events.filter(event => event.userId === +localStorage.activeUser)
-  
-  const eventsByUserSortedByDate = eventsByUser.sort((a,b) => {return new Date(b.eventDate) - new Date(a.eventDate)}) //this is not working
+  const eventsByUser = events.filter(event => event.userId === +localStorage.activeUser).sort((a,b) => {return new Date(b.eventDate) - new Date(a.eventDate)}) 
   
   console.log("eventsByUser", eventsByUser)
-  console.log("eventsByUserSortedByDate", eventsByUserSortedByDate)
+
+  const months = [{id:1, name:"January"},{id:2, name:"February"},{id:3, name:"March"}]
+ 
+
+
   return (
     <>
     <div className="events">
@@ -29,10 +33,21 @@ export const EventList = () => {
         <button id="save-button" className="btn-secondary"onClick={() => {navigate("/events/create")}}>
             Add Event
         </button>
-      {console.log("EventList: Render", events)}
-      <div className="eventList">
+        <div className="eventbyMonth" >   
+        <p>Event sorted by month</p>
 
-      {eventsByUser.length !== 0 ? eventsByUserSortedByDate.map(event => {
+            <select onChange={(taco) =>taco.target.value = events.eventDate.string(5,6)}>
+
+            <option value="0">select</option>
+             {months.map(m =>(
+            <option key={m.id} value={m.id}> {m.name}</option>))}
+            </select>
+            
+      </div> 
+      <div className="eventList">
+        {console.log("EventList: Render", events)}
+
+      {eventsByUser.length !== 0 ?eventsByUser.map(event => {
           return <EventCard key={event.id} event={event} />
         }):<p>No events listed yet</p>
       }
