374f40c313d16f3488fd5b93b3d2fe1bc78c7edc
Nima-ShSh <nimashsh21@gmail.com>
2021-12-17 10:25:06 -0500

    a

--- src/components/Message/MessageCard.js
+++ src/components/Message/MessageCard.js
@@ -2,17 +2,25 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 // import { Link } from "react-router-dom";
 
-export const MessageCard = ({messages}) => {
+export const MessageCard = ({singleMessage, stateChangingFunction, addMessageToState}) => {
     const navigate = useNavigate();
+    
+
+    const onClickHandler = () => {
+        stateChangingFunction(singleMessage.id)
+        addMessageToState(singleMessage)
+      }
+      //singleMessage relates to return in MessageList.js
+
+
+
    return(
     <section className="message">
     <h3 className="message_name">
-      {/* <Link to={`/messages/${messages.userId}`}> */}
-      {messages.user.username}: { }
-        { messages.message }
-        <button onClick={() => {
-    navigate(`/messages/edit/${messages.id}`)}}>Edit</button>
-      {/* </Link> */}
+      {singleMessage.user.username}: { }
+        { singleMessage.message }
+        { singleMessage.userId === +localStorage.activeUser ? <button onClick={onClickHandler}>Edit</button> : ""}
     </h3>
 </section>
    )}
+  
